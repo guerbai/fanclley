f93d9ef1635986b5744bba5bdf36fa76549e6006
guerbai <fyb617243899@gmail.com>
2017-09-11 23:24:38 +0800

    start_server manage done

--- manage.py
+++ manage.py
@@ -5,13 +5,13 @@ from app.models import User
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FANCLLEY_CONFIG') or 'production')
+app = create_app(os.getenv('FANCLLEY_CONFIG') or 'local')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
